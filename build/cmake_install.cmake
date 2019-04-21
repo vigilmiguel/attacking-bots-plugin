0eb0fe3d8db1e62c39534582ffaf17d267d78e46
Miguel Angel <vigil.miguel1019@live.com>
2019-04-20 22:22:18 -0700

    Rebuild with CMake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/vigil/Desktop/Servers/samp-cpp
+# Install script for directory: C:/Users/vigil/Desktop/Servers/attacking-bots-plugin
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/vigil/Desktop/Servers/samp-cpp/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/vigil/Desktop/Servers/attacking-bots-plugin/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
